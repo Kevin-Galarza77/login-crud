f6b9ee407ba340aa4800d659780ecf4c7e5fccac
Kevin-Galarza77 <jimenezkev1040@gmail.com>
2023-06-08 23:36:30 -0500

    Curd listo

--- src/app/crud/crud.page.ts
+++ src/app/crud/crud.page.ts
@@ -8,14 +8,53 @@ import { FirestoreService } from '../services/firestore.service';
 })
 export class CrudPage implements OnInit {
 
-  cars: any[]=[];
+  cars: any[] = [];
 
 
-  constructor(private carService: FirestoreService) {}
+  constructor(private carService: FirestoreService) { }
 
 
   ngOnInit() {
+    this.getCars()
+  }
+
+  getCars() {
+    this.carService.getCars().subscribe({
+      next: result => {
+        this.cars = result;
+      }
+    });
+  }
+
+  updateCar(car_id: any) {
+
+    const color = ['ROJO', 'NEGRO', 'BLANCO', 'GRIS', 'VERDE'];
+    const indiceAleatorio = Math.floor(Math.random() * color.length);
+    const randomWord = color[indiceAleatorio];
+    const placa = ['AAA 1111', 'BBB 2222', 'CCC 3333', 'DDD 4444', 'EEE 5555'];
+    const indiceAleatorioplaca = Math.floor(Math.random() * placa.length);
+    const randomplaca = placa[indiceAleatorioplaca];
+
+    const car = { color: randomWord, placa: randomplaca };
+
+    this.carService.updateCar(car_id, car).then(
+      result => {
+        this.getCars();
+      }
+    ).catch(e => console.log(e));
 
   }
+
+
+  deleteCar(car_id: any) {
+    this.carService.deleteCar(car_id).then(
+      result => {
+        this.getCars();
+        console.log(result);
+      }
+    ).catch(e => console.log(e));
+    console.log(car_id);
+  }
+
 
 }
--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { deleteDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  item$: Observable<any[]>;
+
+  constructor(private firestore: Firestore) {
+    const carsCollection = collection(this.firestore, 'cars'); // Obtener la referencia a la colección "cars"
+    this.item$ = collectionData(carsCollection, { idField: 'id' }); // Obtener un Observable de los documentos con el campo "id" incluido
+  }
+
+  getCars(){
+    return this.item$;
+  }
+
+  createCar(carData: any): Promise<void> {
+    const carsCollection: any = collection(this.firestore, 'cars');
+    return setDoc(doc(carsCollection), carData);
+  }
+
+  updateCar(carId: string, carData: any): Promise<void> {
+    const carDoc = doc(this.firestore, 'cars', carId);
+    return updateDoc(carDoc, carData);
+  }
+
+  deleteCar(carId: string): Promise<void> {
+    const carDoc = doc(this.firestore, 'cars', carId);
+    return deleteDoc(carDoc);
+  }
 
 }
